9de3258c1371eb19f9c023ef0f7c7c8ffb2a155c
marbleheartg <197402001+marbleheartg@users.noreply.github.com>
2025-10-28 00:59:27 +0700

    chore: add hardhat-reown plugin and update dependencies in package.json and hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,10 @@
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+import hardhatReownPlugin from "hardhat-reown";
 import type { HardhatUserConfig } from "hardhat/config";
 // import { configVariable } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxViemPlugin],
+  plugins: [hardhatToolboxViemPlugin, hardhatReownPlugin],
   solidity: {
     version: "0.8.30",
     settings: {
