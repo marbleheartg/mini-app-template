e2acad2e816e774482d9b2684425460ab9b833ea
marbleheartg <197402001+marbleheartg@users.noreply.github.com>
2026-01-08 21:27:33 +0700

    chore

--- app/lib/api/utils/verifySession.ts
+++ app/lib/api/utils/verifySession.ts
@@ -1,13 +1,11 @@
 import { createClient } from "@farcaster/quick-auth"
 
-const { NEXT_PUBLIC_HOST } = process.env
-
 const client = createClient()
 
 export default async function verifySession(session: string): Promise<number> {
   const payload = await client.verifyJwt({
     token: session,
-    domain: NEXT_PUBLIC_HOST!,
+    domain: process.env.NEXT_PUBLIC_HOST!,
   })
 
   return payload.sub
